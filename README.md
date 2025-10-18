50737b733075549bf1559fca70c8e6897d39cf2d
MasterSTime126 <hello.neinbor@gmail.com>
2025-10-19 02:05:08 +0500

    hiearchy

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # -ML-track
-tigers team 
-aiuanats be like:
-
-
-P.S. We will write here a description of our project, so don't delete this file.
+tigers team
--- scripts/input_handler.py
+++ scripts/input_handler.py
@@ -1,3 +1,7 @@
-def get_prompt():
-	#Just for future, there might be changes for website or app versions.
-	return input("Write your prompt here: ")
+#input_handler.py
+#
+#The function of this code is to handle app and web inputs, so in those cases this code would be bigger...
+
+def get_prompt(prompt_description: str = "") -> str:
+	#Just simple. No offence to my code.
+	return input(prompt_description)
